b2ec8c50ab77870a52a0840c5832eff3b006bd77
Ali Shah <ali1195shah@gmail.com>
2019-09-20 20:35:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+# code here
+require 'pry'
+
+class School
+  
+  attr_reader :roster, :add_student, :grade
+  attr_reader
+  
+  def initialize(roster)
+    @roster = {}
+  end
+  
+  def add_student(student_name, grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << student_name
+    else
+      @roster[grade] = [student_name]
+    end
+  end
+  
+  def grade(grade)
+    
+  end
+  
+end
